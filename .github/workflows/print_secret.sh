0f8583f0e7ba1fb2bfe77b95a0cfd09c4aa580a6
chandani <chandani@stepfunction.ai>
2026-01-16 23:02:09 +0530

    scret manager testing

--- .github/workflows/print_secret.sh
+++ .github/workflows/print_secret.sh
@@ -6,5 +6,6 @@ if [ -z "$MY_SECRET_KEY" ]; then
   echo "❌ Secret is NOT available"
 else
   echo "✅ Secret is available"
+  echo "Secret: $MY_SECRET_KEY"
   echo "Secret length: ${#MY_SECRET_KEY}"
 fi
